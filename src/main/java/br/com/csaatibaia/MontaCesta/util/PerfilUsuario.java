7142db27125cf2ab77dc181ff69f0850d6e78892
vinidesenvolve <vinidesenvolve@gmail.com>
2021-08-05 11:27:02 -0300

    Created Enums 'TipoCesta' and 'PerfilUsuario'

---
+++ src/main/java/br/com/csaatibaia/MontaCesta/util/PerfilUsuario.java
@@ -0,0 +1,8 @@
+package br.com.csaatibaia.MontaCesta.util;
+
+public enum PerfilUsuario {
+    
+    ADMIN,
+    GERENTE,
+    COAGRI
+}
---
+++ src/main/java/br/com/csaatibaia/MontaCesta/util/TipoCesta.java
@@ -0,0 +1,9 @@
+package br.com.csaatibaia.MontaCesta.util;
+
+public enum TipoCesta {
+
+    QUATRO,
+    OITO,
+    DOZE
+    
+}
